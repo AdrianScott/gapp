100e72a0f06d5c8575250a91365f9492593c3430
Ira Miller <public@iramiller.com>
2017-06-07 11:56:52 -0500

    improve init efficiency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 'use strict'
+const os = require('os')
+const path = require('path')
+const perspective = require('./perspective.js')
 
 module.exports = {
   'gapp': require('./gapp.js'),
   'gpg': require('./gpg.js'),
   'github': require('./github.js'),
-  'perspective': require('./perspective.js')
+  'perspective': perspective,
+  GULDHOME: perspective.GULDHOME || path.join(os.homedir(), 'blocktree')
 }
--- src/perspective.js
+++ src/perspective.js
@@ -28,6 +28,8 @@ var cfgget = () => {
 }
 
 module.exports = class Perspective {
+  GULDHOME = GULDHOME
+
   constructor () {
     this.path = GULDHOME
     process.chdir(this.path)
@@ -39,9 +41,8 @@ module.exports = class Perspective {
           self.name = config.user.username
           self.email = config.user.email
           self.fingerprint = config.user.signingkey
-          self.loadConfig(self.fingerprint).then((config) => {
-            console.log(config)
-            self.github = github.getGithub({}, config.githubOAUTH)
+          Perspective.loadConfig(self.fingerprint).then((config) => {
+            self.github = github.getGithub({}, JSON.parse(config.toString('utf-8')).githubOAUTH)
           })
         } else throw new Error('No perspective found, run setup.')
       })
@@ -71,8 +72,33 @@ module.exports = class Perspective {
     })
   }
 
-  static init (username, pass, email, fingerprint) {
+  static startInit (username, pass, email, fingerprint) {
     var git
+    gitCfg = {
+      'user.username': username,
+      'user.email': email,
+      'commit.gpgsign': true
+    }
+    var initDirs = () => {
+      process.chdir(GULDHOME)
+      git = Git(GULDHOME).init()
+      return fs.mkdirs('life/' + username)
+      .then(() => { fs.mkdirs('media') })
+      .then(() => { fs.mkdirs('tech') })
+      .then(() => { fs.mkdirs('keys/pgp') })
+      .then(() => { fs.writeFile('.gitignore', 'node_modules\nconfig.json*') })
+      .then(() => {
+        git.add('./*')
+        .submoduleAdd('https://github.com/isysd/vrcave.git', 'tech/js/guld-vrcave')
+        .then(Promise.resolve)
+      })
+    }
+    return fs.mkdirs(GULDHOME).then(initDirs)
+  }
+
+  static finishInit(username, pass, email, fingerprint) {
+    process.chdir(GULDHOME)
+    var git = Git(GULDHOME)
     gitCfg = {
       'user.username': username,
       'user.email': email,
@@ -80,7 +106,7 @@ module.exports = class Perspective {
     }
     var getFingSafe = () => {
       return new Promise((resolve, reject) => {
-        if (fingerprint) gitCfg['user.signingkey'] = resolve(fingerprint)
+        if (fingerprint) resolve(fingerprint)
         else {
           gpg.genKey(username, email)
           .then(() => {
@@ -89,62 +115,53 @@ module.exports = class Perspective {
         }
       })
     }
-
-    var initAll = () => {
-      process.chdir(GULDHOME)
-      git = Git(GULDHOME).init()
-      return fs.mkdirs('life/' + username)
-      .then(() => { fs.mkdirs('media') })
-      .then(() => { fs.mkdirs('tech') })
-      .then(() => { fs.mkdirs('keys/pgp') })
-      .then(() => { fs.writeFile('.gitignore', 'node_modules\nconfig.json*') })
-      .then(() => { git.add('.gitignore') })
-      .then(getFingSafe)
-      .then(addKey)
-    }
     var addKey = (fingerprint) => {
       gitCfg['user.signingkey'] = fingerprint
       gitconfig.set(gitCfg, {location: 'global'})
-      return new Promise((resolve, reject) => {
-        fs.writeFile('life/' + username + '/gap.json', JSON.stringify({
+      return fs.writeFile('life/' + username + '/gap.json', JSON.stringify({
           'name': username,
           'fingerprint': fingerprint,
           'observer': username
-        }, null, 2)).then(() => {
-          git.add('life')
-          gpg.gpg.call('', ['--export', '-a', fingerprint], (err, data) => {
-            if (err) reject(err)
-            else {
-              var keypath = path.join('keys', 'pgp', username + '.asc')
-              fs.writeFile(keypath, data.toString('utf-8')).then(() => {
-                git.add(keypath)
-                github.createGHToken({username: username, password: pass})
-                .then((token) => {
-                  Perspective.saveConfig({'githubOAUTH': token}, fingerprint).then(() => {
-                    var myGH = github.getGithub({}, token)
-                    myGH.repos.create({
-                      'name': username,
-                      'description': 'Blocktree perspective of ' + username,
-                      'private': false,
-                      'has_issues': false,
-                      'has_projects': false,
-                      'has_wiki': false
-                    }).then(() => {
-                      git.addRemote(username, 'https://github.com/' + username + '/' + username + '.git')
-                      .checkoutLocalBranch(username)
-                      .commit('Initialize perspective for ' + username)
-                      .push(username, username, ['-f']).then(resolve)
-                    })
-                  })
+        }, null, 2))
+    }
+    return getFingSafe()
+    .then(addKey)
+    .then(new Promise((resolve, reject) => {
+      gpg.gpg.call('', ['--export', '-a', fingerprint], (err, data) => {
+        if (err) reject(err)
+        else {
+          var keypath = path.join('keys', 'pgp', username + '.asc')
+          fs.mkdirs(path.join(GULDHOME, 'keys', 'pgp'))
+          .then(fs.writeFile(path.join(GULDHOME, keypath), data.toString('utf-8'))).then(() => {
+            git.add('./*')
+            github.createGHToken({username: username, password: pass})
+            .then((token) => {
+              Perspective.saveConfig({'githubOAUTH': token}, fingerprint).then(() => {
+                var myGH = github.getGithub({}, token)
+                myGH.repos.create({
+                  'name': username,
+                  'description': 'Blocktree perspective of ' + username,
+                  'private': false,
+                  'has_issues': false,
+                  'has_projects': false,
+                  'has_wiki': false
+                }).then(() => {
+                  // TODO use SSH instead of this netrc + OAUTH crap
+                  var netrc = require('netrc')
+                  var myNetrc = netrc()
+                  myNetrc['github.com'] = {'login': username, 'password': token}
+                  netrc.save(myNetrc)
+                  git.addRemote(username, 'https://github.com/' + username + '/' + username + '.git')
+                  .checkoutLocalBranch(username)
+                  .commit('Initialize perspective for ' + username)
+                  .push(username, username, ['-f']).then(resolve)
                 })
               })
-            }
+            })
           })
-        })
+        }
       })
-    }
-
-    return fs.mkdirs(GULDHOME).then(initAll)
+    }))
   }
 
   // installJS(name) {
